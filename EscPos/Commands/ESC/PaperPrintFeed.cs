975fc36759ba55f0b5b4dde90ee7f59dc76c8553
DESKTOP-GC983K8\Leo <lmuffatti@gmail.com>
2026-06-21 22:04:58 +0200

    Correction of 1B 4A command codified as 1B 6A by page bug at https://escpos.readthedocs.io/en/latest/paper_movement.html#print-and-feed-paper-n-lines-1b-64-phx

--- EscPos/Commands/ESC/PaperPrintFeed.cs
+++ EscPos/Commands/ESC/PaperPrintFeed.cs
@@ -10,7 +10,7 @@ namespace ReceiptPrinterEmulator.EscPos.Commands.ESC;
 /// </summary>
 public class PaperPrintFeed : BaseCommand
 {
-	public override string Prefix => EscPosInterpreter.ESC + "j";
+	public override string Prefix => EscPosInterpreter.ESC + "J";
 	public override bool HasArgs => true;
 
 	private byte _n;
